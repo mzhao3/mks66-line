057fd671c340fa9e2cef3964c533dc380fbb4dae
Maggie <mzhao3@stuy.edu>
2019-02-08 15:19:25 -0500

    making lines

---
+++ draw.py
@@ -0,0 +1,60 @@
+from display import *
+
+def draw_line( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x0 - x1
+
+    # octant 1
+    if (A <= (-1 * B) and A >= 0):
+        D = 2*A + B
+        while (x <= x1):
+            plot(screen, color, x, y)
+            if (D > 0):
+                y += 1
+                D += 2*B
+            x += 1
+            D += 2*A
+
+    # octant 2
+    elif (A > (-1 * B) and B <= 0 ):
+        D = A + 2 * B
+        while (y <= y1):
+            plot(screen, color, x, y)
+            if (D < 0):
+                x += 1
+                D += 2*A
+            y += 1
+            D += 2*B
+
+    # octant 7
+    if (A < 0):
+        #print("the difference in y:" + str(A))
+        #print("the difference in x:" + str(B))
+        if (A < B):
+            print("in octant 7")
+            D = A + 2 * B
+            while (y >= y1):
+                #print(y)
+                plot(screen, color, x, y)
+                if (D > 0):
+                    x += 1
+                    D += 2*A
+                y -= 1
+                D += -2*B
+
+
+        #fix
+        elif ((-1 * A) <= (-1 * B)):
+            print("in octant 8")
+            D = -2*A + B
+            while (x <= x1):
+                plot(screen, color, x, y)
+                if (D > 0):
+                    y -= 1
+                    D += 2*B
+                x += 1
+                D += -2*A
+
+    return
---
+++ main.py
@@ -0,0 +1,58 @@
+from display import *
+from draw import *
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+'''
+draw_line(250, 250, 500, 500, screen, [255,0,0])
+draw_line(250, 250, 500, 0, screen, [255,0,0])
+
+draw_line(0, 250, 500, 250, screen, [0,0,255])
+draw_line(250, 0, 250, 500, screen, [0,0,255])
+
+draw_line(100, 100, 200, 0, screen, color)
+draw_line(250, 250, 500, 230, screen, color)
+draw_line(250, 250, 300, 0, screen, color)
+'''
+x0 = 0
+y0 = 0
+x1 = 0
+y1 = 500
+x2 = 500
+y2 = 500
+x3 = 500
+y3 = 0
+i = 0
+diffa = 10
+diffb = 6
+while (i < 100) :
+    draw_line(x1, y1, x0, y0, screen, [255-x0/2, 255-y0/2, x0+y0/4])
+    draw_line(x1, y1, x2, y2, screen, [255-x1/2, 255-y1/2, x1+y1/4])
+    draw_line(x2, y2, x3, y3, screen, [255-x2/2, 255-y2/2, x2+y2/4])
+    draw_line(x0, y0, x3, y3, screen, [255-x3/2, 255-y3/2, x1+y3/4])
+    x0 += diffa
+    y0 += diffb
+    x1 += diffb
+    y1 -= diffa
+    x2 -= diffa
+    y2 -= diffb
+    x3 -= diffb
+    y3 += diffa
+    i += 1
+    if (i % 8 == 0):
+        diffa -= 1
+        diffb -= 1
+
+
+#uncomment this later
+#display(screen)
+save_extension(screen, 'img.png')
+
+
+def draw_square(x0, y0, x1, y1, len, color):
+    A = y1 - y0
+    B = x1 - x0
+    draw_line(x0, y0, x1, y1, screen, color)
+    draw_line
+
+    return
